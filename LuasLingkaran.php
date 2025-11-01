9c6b617ec9011a7f8d5747ccc34c777fcc79c944
ALDIANSYAH2704 <aldi87223@gmail.com>
2025-11-01 12:49:55 +0700

    pertemuan3

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,30 +1,13 @@
 <?php
 
-class LuasLingkaran{
-
+class LuasLingkaran {
+    
     public const phi = 3.14;
     public int $jari;
-
-    PUBLIC function __construct($isijari = 1) {
-        $this->jari = $isijari;
-    }
-
-    public function tampil($nama = 'ban') {
-        $rumus = LuasLingkaran::phi * $this ->jari * $this->jari;
-        echo "lingkaran {$nama} Hasilnya adalah: {$rumus} ";
-    }
-
-    public static function testing() {
-        echo "<br/>";
-        echo "ini dari static";
-    }
-
-    public function __destruct(){
-       echo "<br/>";
-       echo "udah ah cape";
-    }
 }
-    
-$lingkaran = new LuasLingkaran(7);
-$lingkaran->tampil('roda'); //panggil method
-LuasLingkaran::testing();
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 7;
+
+$rumus = LuasLingkaran::phi * $lingkaran->jari * $lingkaran->jari;
+echo "Hasilnya adalah: ". $rumus;
