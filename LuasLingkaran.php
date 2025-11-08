abb4c04e8d1d4892351897f4cbe502f30edfc47c
ALDIANSYAH2704 <aldi87223@gmail.com>
2025-11-08 13:50:11 +0700

    pertemuan6

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,17 +1,19 @@
 <?php
 
-class LuasLingkaran{
+namespace App\Math; //pasang namespace
 
+class LuasLingkaran {
+    
     public const phi = 3.14;
-    public int $jari;
+    private int $jari;
 
-    PUBLIC function __construct($isijari = 1) {
-        $this->jari = $isijari;
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari;
     }
 
     public function tampil($nama = 'ban') {
-        $rumus = LuasLingkaran::phi * $this ->jari * $this->jari;
-        echo "lingkaran {$nama} Hasilnya adalah: {$rumus} ";
+        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
     }
 
     public static function testing() {
@@ -19,12 +21,10 @@ class LuasLingkaran{
         echo "ini dari static";
     }
 
-    public function __destruct(){
-       echo "<br/>";
-       echo "udah ah cape";
+    public function __destruct() {
+        echo "<br/>";
+        echo "udah ah cape";
     }
 }
-    
-$lingkaran = new LuasLingkaran(7);
-$lingkaran->tampil('roda'); //panggil method
-LuasLingkaran::testing();
+
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('./LuasLingkaran.php'); //panggil file LuasLingkaran.php
+
+use App\Math\LuasLingkaran; //panggil namespace
+
+$lingkaran = new LuasLingkaran(10);
+$lingkaran->tampil('roda'); //panggil method
+LuasLingkaran::testing();
